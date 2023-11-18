2aa55b9b626231a3a210ce2efcf5f255faa8c74a
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-11-19 00:03:13 +0330

    edit contact admin.py

--- Contact/admin.py
+++ Contact/admin.py
@@ -5,5 +5,4 @@ from . import models
 class showContact(admin.ModelAdmin):
     list_display = ["full_name", 'email', 'phone_number', 'message', 'datetime']
 
-
 admin.site.register(models.Contact, showContact)
