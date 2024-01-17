8d5bd981daf8155d1f9a26df79a536f9307c9422
MrMrProgrammer <mr.mr.programmer@gmail.com>
2024-01-17 10:58:26 +0330

    add models in resume app

--- Resume/views.py
+++ Resume/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from .models import Skill, Language, Experience
+from .models import Skill, Language, Experience, Education, ResumeFile
 
 
 def Resume(request):
     skills = Skill.objects.filter(is_active=True).order_by('-priority')
     languages = Language.objects.filter(is_active=True).order_by('-priority')
     experiences = Experience.objects.filter(is_active=True).order_by('-priority')
+    educations = Education.objects.filter(is_active=True).order_by('-id')
+    resume_file = ResumeFile.objects.filter(is_active=True).last()
 
     context = {
         'skills': skills,
         'languages': languages,
         'experiences': experiences,
+        'educations': educations,
+        'resume_file': resume_file,
     }
 
     return render(request, 'Resume/resume.html', context)
