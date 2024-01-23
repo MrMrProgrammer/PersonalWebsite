e41f65fed7614fc9018a26b2dfffb756467d4302
MrMrProgrammer <mr.mr.programmer@gmail.com>
2024-01-23 16:36:30 +0330

    Improved several features and facilities

--- Contact/admin.py
+++ Contact/admin.py
@@ -3,6 +3,11 @@ from . import models
 
 
 class showContact(admin.ModelAdmin):
-    list_display = ["full_name", 'email', 'phone_number', 'message', 'datetime']
+    list_display = ["full_name", 'email', 'phone_number', 'message', 'formatted_datetime']
+
+    def formatted_datetime(self, obj):
+        return obj.datetime.strftime('%Y-%m-%d | %H:%M')
+
+    formatted_datetime.short_description = 'Date & Time'
 
 admin.site.register(models.Contact, showContact)
