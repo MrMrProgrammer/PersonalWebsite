ece5622c0f69cb5fb44993f8351abbf972b562e2
MrMrProgrammer <mr.mr.programmer@gmail.com>
2024-01-23 16:36:50 +0330

    Improved several features and facilities

--- Log/models.py
+++ Log/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Logger(models.Model):
+    ip = models.CharField(max_length=20)
+    count = models.IntegerField(default=1)
     date = models.CharField(max_length=20)
     time = models.CharField(max_length=20)
-    ip = models.CharField(max_length=20)
     agent = models.CharField(max_length=500)
     language = models.CharField(max_length=100)
+    client_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
